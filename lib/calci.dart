761dca897ce38a5105eeb3a7469527e4b4a955c5
Hemant Kumar <hemantbanjara855@gmail.com>
2023-03-06 20:19:51 +0530

    Calci

---
+++ lib/calci.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library calci;
+
+export 'src/calculations.dart' show Calculations;
+
+// TODO: Export any libraries intended for clients of this package.
