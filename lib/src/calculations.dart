c0d08ba13fa208214f8a668048dc9875f40e6505
Hemant Kumar <hemantbanjara855@gmail.com>
2023-03-06 20:18:21 +0530

    calci

---
+++ lib/src/calculations.dart
@@ -0,0 +1,10 @@
+// TODO: Put public facing types in this file.
+
+/// Checks if you are awesome. Spoiler: you are.
+class Calculations {
+  int a, b;
+
+  int add() => a+b;
+
+  Calculations(this.a, this.b);
+}
