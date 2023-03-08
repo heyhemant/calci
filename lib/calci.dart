10480cf2e6d3355e587b076e4d146081b2d2b371
heyhemant <62196986+heyhemant@users.noreply.github.com>
2023-03-08 22:15:48 +0530

    new js

--- lib/calci.dart
+++ lib/calci.dart
@@ -1,8 +1,11 @@
 /// Support for doing something awesome.
 ///
-/// More dartdocs go here.
+/// More dartdocs go here
+@JS()
 library calci;
 
+import 'dart:js_interop';
+@JS()
 export 'src/calculations.dart' show Calculations;
 
 // TODO: Export any libraries intended for clients of this package.
